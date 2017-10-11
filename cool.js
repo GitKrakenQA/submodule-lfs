29c599ce1d4726334e90e705f9ec0545de0950a3
Keif <gitkrakenqa@gmail.com>
2017-10-11 10:22:13 -0700

    Commit dat shiz

--- cool.js
+++ cool.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:908bdcd0b80051009b49bb38f4d30bd1bd29db1ffb00e2c93113977864f0e1a2
-size 97
+oid sha256:5a136a4d7976a66bc0bc8150a1a6f66aef16febf813aa4639eed444e3b7b7804
+size 196
